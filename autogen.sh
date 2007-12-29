4fe6610368def83e1f60b489e402b467183dd905
Tollef Fog Heen <tfheen@err.no>
2007-12-29 14:40:41 +0100

    2007-12-29  Tollef Fog Heen  <tfheen@err.no>
    	* Update to glib 1.2.10, adjust lots of the glib patches.  Gnome
    	#81847.

--- autogen.sh
+++ autogen.sh
@@ -55,29 +55,30 @@ test $TEST_TYPE $FILE || {
 	exit 1
 }
 
-rm -r glib-1.2.8
-gunzip --stdout glib-1.2.8.tar.gz | tar xf - || { 
+rm -r glib-1.2.10
+gunzip --stdout glib-1.2.10.tar.gz | tar xf - || { 
     echo "glib tarball not unpacked"
     exit 1
 }
 
-chmod +w `find glib-1.2.8 -name Makefile.am`
-perl -p -i.bak -e "s/lib_LTLIBRARIES/noinst_LTLIBRARIES/g" `find glib-1.2.8 -name Makefile.am`
-perl -p -i.bak -e "s/bin_SCRIPTS/noinst_SCRIPTS/g" `find glib-1.2.8 -name Makefile.am`
-perl -p -i.bak -e "s/include_HEADERS/noinst_HEADERS/g" `find glib-1.2.8 -name Makefile.am`
-perl -p -i.bak -e "s/[a-zA-Z0-9]+_DATA/noinst_DATA/g" `find glib-1.2.8 -name Makefile.am`
-perl -p -i.bak -e "s/info_TEXINFOS/noinst_TEXINFOS/g" `find glib-1.2.8 -name Makefile.am`
-perl -p -i.bak -e "s/man_MANS/noinst_MANS/g" `find glib-1.2.8 -name Makefile.am`
+chmod +w `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e "s/lib_LTLIBRARIES/noinst_LTLIBRARIES/g" `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e "s/bin_SCRIPTS/noinst_SCRIPTS/g" `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e "s/include_HEADERS/noinst_HEADERS/g" `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e "s/glibnoinst_HEADERS/noinst_HEADERS/g" `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e 's/([a-zA-Z0-9]+)_DATA/noinst_DATA/g' `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e "s/info_TEXINFOS/noinst_TEXINFOS/g" `find glib-1.2.10 -name Makefile.am`
+perl -p -i.bak -e "s/man_MANS/noinst_MANS/g" `find glib-1.2.10 -name Makefile.am`
 
 ## patch gslist.c to have stable sort
-perl -p -w -i.bak -e 's/if \(compare_func\(l1->data,l2->data\) < 0\)/if \(compare_func\(l1->data,l2->data\) <= 0\)/g' glib-1.2.8/gslist.c
+perl -p -w -i.bak -e 's/if \(compare_func\(l1->data,l2->data\) < 0\)/if \(compare_func\(l1->data,l2->data\) <= 0\)/g' glib-1.2.10/gslist.c
 
 # Update random auto* files to actually have something which have a snowball's
 # chance in a hot place of working with modern auto* tools.
 
-(cd glib-1.2.8 && for p in ../glib-patches/*.diff; do echo $p; patch -p1 < $p || exit 1; done ) || exit 1
+(cd glib-1.2.10 && for p in ../glib-patches/*.diff; do echo $p; patch -p1 < $p || exit 1; done ) || exit 1
 
-(cd glib-1.2.8 && libtoolize --copy --force && $ACLOCAL $ACLOCAL_FLAGS && $AUTOMAKE && $AUTOCONF) || exit 1
+(cd glib-1.2.10 && libtoolize --copy --force && $ACLOCAL $ACLOCAL_FLAGS && $AUTOMAKE && $AUTOCONF) || exit 1
 
 if test -z "$*"; then
 	echo "I am going to run ./configure with no arguments - if you wish "
