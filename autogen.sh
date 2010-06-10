84e4847c06bd8f14271f1e46c7f80a33034c9a92
Dan Nicholson <dbn.lists@gmail.com>
2010-06-09 21:28:55 -0700

    Greatly simplify autogen.sh and fix configure skipping
    There is really no reason to manually vet and run all the autotools.
    That's what autoreconf is for. It has the added bonus that it will
    descend to the glib subdirectory and rebuild the autotools there. The
    handling of configure is also fixed as the previous --no-configure code
    was completely broken.
    
    This is basically the xorg autogen.sh, so we can be pretty confident
    it'll work as advertised.

--- autogen.sh
+++ autogen.sh
@@ -1,59 +1,12 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
+PROJECT=pkg-config
+srcdir=`dirname "$0"`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
 cd $srcdir
-
-PROJECT=pkg-config
-TEST_TYPE=-f
-FILE=pkg.m4
-
-DIE=0
-
-AUTOCONF=autoconf2.50
-AUTOHEADER=autoheader2.50
-
-($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
-        AUTOCONF=autoconf
-        AUTOHEADER=autoheader
-}
-
-
-($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
-
-AUTOMAKE=automake-1.7
-ACLOCAL=aclocal-1.7
-
-($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
-        AUTOMAKE=automake
-        ACLOCAL=aclocal
-}
-
-($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-test $TEST_TYPE $FILE || {
-	echo "You must run this script in the top-level $PROJECT directory"
-	exit 1
-}
 
 rm -r glib-1.2.10
 gunzip --stdout glib-1.2.10.tar.gz | tar xf - || { 
@@ -78,35 +31,13 @@ perl -p -w -i.bak -e 's/if \(compare_func\(l1->data,l2->data\) < 0\)/if \(compar
 
 (cd glib-1.2.10 && for p in ../glib-patches/*.diff; do echo $p; patch -p1 < $p || exit 1; done ) || exit 1
 
-(cd glib-1.2.10 && libtoolize --copy --force && $ACLOCAL $ACLOCAL_FLAGS && $AUTOMAKE && $AUTOCONF) || exit 1
-
-run_configure=yes
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-else
-    case "--no-configure" in
-	"*$") run_configure=no
-    esac
-fi
-
-libtoolize --copy --force
-
-echo $ACLOCAL $ACLOCAL_FLAGS
-$ACLOCAL $ACLOCAL_FLAGS
-
-# optionally feature autoheader
-($AUTOHEADER --version)  < /dev/null > /dev/null 2>&1 && $AUTOHEADER
-
-$AUTOMAKE -a $am_opt
-$AUTOCONF
+# Rebuild the autotools for pkg-config and glib
+${AUTORECONF-autoreconf} -iv || exit $?
 
 cd $ORIGDIR
 
-if [ "$run_configure" = "yes" ]; then
-    export AUTOMAKE
-    export ACLOCAL
-    $srcdir/configure --enable-maintainer-mode --disable-shared --disable-threads "$@"
+if [ -z "$NOCONFIGURE" ] && [ "$1" != --no-configure ]; then
+    "$srcdir"/configure --enable-maintainer-mode "$@"
     echo 
     echo "Now type 'make' to compile $PROJECT."
 else
